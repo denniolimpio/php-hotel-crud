d945aa9651c3dfce7bbe0383f6b2b0a137a5af80
dennis <dennis.olimpio0794@gmail.com>
2020-07-28 16:30:59 +0200

    file base

---
+++ database.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "dbhotel";
+
+// Connect
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn && $conn->connect_error) {
+  
+echo "Connection failed: " . $conn->connect_error;
+
+}
+?>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!-- pagina pricipale -->
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title> hotel crud</title>
+
+<link rel="stylesheet" href="dist/app.css">
+</head>
+<body>
+
+  <header>
+    <h1> Hotel Boolean</h1>
+  </header>
+
+<!-- lista stanze -->
+  <main>
+
+    <table>
+      <thead>
+        <tr>
+          <th> ID</th>
+          <th> Numero Stanza</th>
+          <th> Piano</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <!-- stanza -->
+        <tr class="singola_stanza">
+          <td> 1</td>
+          <td>41</td>
+          <td> 4</td>
+        </tr>
+        <!-- fine stanza -->
+
+      </tbody>
+    </table>
+    <!-- Fine lista stanze -->
+
+  </main>
+</body>
+</html>
